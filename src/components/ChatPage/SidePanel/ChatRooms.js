34ffbf46a3a925432f2604c9bdb68b78d2cd277b
Bruce Choe <whdtmek@gmail.com>
2021-10-19 11:11:43 +0900

    add: chatrooms UI 추가

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaRegSmileWink, FaPlus } from 'react-icons/fa';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const ChatRooms = () => {
-  return <div>ChatRooms</div>;
-};
+export class ChatRooms extends Component {
+  state = {
+    show: false,
+  };
+
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
+
+  render() {
+    return (
+      <div>
+        <div
+          className=''
+          style={{
+            position: 'relative',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <FaRegSmileWink style={{ marginRight: 3 }} />
+          Chat Rooms
+          <FaPlus
+            style={{ position: 'absolute', right: 0, cursor: 'pointer' }}
+            onClick={this.handleShow}
+          />
+        </div>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a chat room</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className='mb-3' controlId='formBasicEmail'>
+                <Form.Label>방 이름</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter a chat room name'
+                />
+              </Form.Group>
+
+              <Form.Group className='mb-3' controlId='formBasicPassword'>
+                <Form.Label>방 설명</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter a chat room description '
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant='primary' onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 export default ChatRooms;
--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -56,6 +56,14 @@ const UserPanel = () => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
       },
       (error) => {
         console.log('업로드 안됨', error);
